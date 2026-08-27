8bfd26b794f3d241b34f352c0a232d2f517d914a
shirlycanaveral67020-jpg <shirly.canaveral67020@ucaldas.edu.co>
2026-08-27 16:37:26 -0500

    docs

--- src/app/servicios/comidas.ts
+++ src/app/servicios/comidas.ts
@@ -9,17 +9,16 @@ export class ComidasService {
 
   constructor(private http: HttpClient) {}
 
-  // Consulta inicial para traer comidas al cargar la página
+  
   obtenerTodasComidas(): Observable<any> {
     return this.http.get('https://www.themealdb.com/api/json/v1/1/search.php?s=chicken');
   }
 
-  // Buscar comida por nombre
   obtenerPorNombre(nombre: string): Observable<any> {
     return this.http.get('https://www.themealdb.com/api/json/v1/1/search.php?s=' + nombre);
   }
 
-  // Buscar comida por ingrediente
+  
   obtenerPorIngrediente(ingrediente: string): Observable<any> {
     return this.http.get('https://www.themealdb.com/api/json/v1/1/filter.php?i=' + ingrediente);
   }
